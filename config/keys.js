e5c6081711057e7b26cd9db1b5d398c2333421d8
Vaasu Dhand <dhandvaasu@gmail.com>
2020-07-28 00:15:34 -0400

    Deployed to Heroku

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MongoURI: 'mongodb+srv://vaasudhand:vaasuatlas@testcluster1.3lkci.mongodb.net/Auth?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
+const flash = require('connect-flash')
+const session = require('express-session')
+const passport = require('passport')
+
+const app = express()
+
+// Passport config
+require('./config/passport')(passport)
+
+// DB Config
+const db = require('./config/keys').MongoURI;
+
+// Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.error(err))
+
+// 3rd Parth Middleware comes with the next method already invoked
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }))
+
+// Express Session Middleware
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+}))
+
+// Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Connect Flash
+app.use(flash())
+
+// Global Variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  res.locals.error = req.flash('error')
+  next()
+})
+
+// EJS (Order matters)
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
+
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/users', require('./routes/users'))
+
+app.listen(process.env.PORT || 3000, console.log(`Server started on port ${3000}`))
